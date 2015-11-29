dc219cd54070727fafc6ec832b01ec8c862b5605
Bozhidar Batsov <bozhidar@batsov.com>
2015-11-29 10:34:43 +0200

    Bump the clojure-mode dep

--- inf-clojure.el
+++ inf-clojure.el
@@ -7,7 +7,7 @@
 ;; URL: http://github.com/clojure-emacs/inf-clojure
 ;; Keywords: processes, clojure
 ;; Version: 1.3.0
-;; Package-Requires: ((emacs "24.1") (clojure-mode "4.0"))
+;; Package-Requires: ((emacs "24.1") (clojure-mode "5.0"))
 
 ;; This file is part of GNU Emacs.
 
