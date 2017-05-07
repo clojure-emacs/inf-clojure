e14dbba5a4ebc80d4d855b174dc1a1aede1af73f
Bozhidar Batsov <bozhidar@batsov.com>
2017-05-07 12:33:34 +0300

    Update the commentary section of the package

--- inf-clojure.el
+++ inf-clojure.el
@@ -26,7 +26,30 @@
 
 ;;; Commentary:
 ;;
-;; inferior-lisp adapted for Clojure.
+;; This package provides basic interaction with a Clojure subprocess (REPL).
+;; It's based on ideas from the popular `inferior-lisp` package.
+;;
+;; `inf-clojure` has two components - a nice Clojure REPL with
+;; auto-completion and a minor mode (`inf-clojure-minor-mode`), which
+;; extends `clojure-mode` with commands to evaluate forms directly in the
+;; REPL.
+;;
+;; `inf-clojure` provides a set of essential features for interactive
+;; Clojure(Script) development:
+;;
+;; * REPL
+;; * Interactive code evaluation
+;; * Code completion
+;; * Definition lookup
+;; * Documentation lookup
+;; * ElDoc
+;; * Apropos
+;; * Macroexpansion
+;; * Support connecting to socket REPLs
+;; * Support for Lumo
+;; * Support for Planck
+;;
+;; For a more powerful/full-featured solution see https://github.com/clojure-emacs/cider.
 ;;
 ;; If you're installing manually, you'll need to:
 ;;
