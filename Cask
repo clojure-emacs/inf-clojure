ff633530fc187b67f41370067bd23c1a5f2dd6a9
Andrea Richiardi <a.richiardi.work@gmail.com>
2018-01-22 15:55:23 -0800

    Add test harness (finally?)
    This patch add ert test harness and some tests around the
    inf-clojure-completion-bounds-of-expr-at-point function.

---
+++ Cask
@@ -0,0 +1,11 @@
+(source gnu)
+(source melpa)
+
+(package-file "inf-clojure.el")
+
+(files "*.el" (:exclude ".dir-locals.el"))
+
+(development
+ (depends-on "clojure-mode")
+ (depends-on "buttercup")
+ (depends-on "assess"))
---
+++ test/inf-clojure-tests.el
@@ -0,0 +1,114 @@
+;;; inf-clojure-tests.el --- Tests for Inf-Clojure -*- lexical-binding: t; -*-
+;;
+;; Copyright © 2014-2018 Bozhidar Batsov
+
+;; Authors: Bozhidar Batsov <bozhidar@batsov.com>
+;;          Andrea Richiardi <a.richiardi.work@gmail.com>
+;; URL: http://github.com/clojure-emacs/inf-clojure
+;; Keywords: processes, clojure
+;; Package-Requires: ((emacs "24.4") (clojure-mode "5.3"))
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Code completion using alexander-yakushev/compliment.
+
+;;; Code:
+
+;; Tests for inf-clojure.el
+
+(message "Running tests on Emacs %s" emacs-version)
+
+(require 'buttercup)
+(require 'assess)
+(require 'inf-clojure)
+
+(cl-defmacro ict-with-assess-buffers ((&rest varlist) &body body)
+  `(assess-with-temp-buffers (,@varlist)
+                             (clojure-mode)
+                             (inf-clojure-minor-mode)
+                             ,@body))
+
+(defun ict-bounds-string (bounds)
+  (buffer-substring (car bounds) (cdr bounds)))
+
+(describe "inf-clojure--kw-to-symbol"
+  (it "returns symbol form of the given keyword"
+    (expect (inf-clojure--kw-to-symbol "symbol") :to-equal "symbol")
+    (expect (inf-clojure--kw-to-symbol ":clj.core/str") :to-equal "clj.core/str")
+    (expect (inf-clojure--kw-to-symbol "::keyword") :to-equal "keyword")
+    (expect (inf-clojure--kw-to-symbol nil) :to-equal nil)))
+
+(describe "completion bounds at point" ()
+  (it "computes bounds for plain-text"
+      (ict-with-assess-buffers
+       ((a (insert "plain-text")))
+       (with-current-buffer a
+         (expect (ict-bounds-string (inf-clojure-completion-bounds-of-expr-at-point))
+                 :to-equal "plain-text"))))
+
+  (it "computes bounds for @deref"
+      (ict-with-assess-buffers
+       ((a (insert "@deref")))
+       (with-current-buffer a
+         (expect (ict-bounds-string (inf-clojure-completion-bounds-of-expr-at-point))
+                 :to-equal "deref"))))
+
+  (it "computes bounds for ^:keyword"
+      (ict-with-assess-buffers
+       ((a (insert "^:keyword")))
+       (with-current-buffer a
+         (expect (ict-bounds-string (inf-clojure-completion-bounds-of-expr-at-point))
+                 :to-equal ":keyword"))))
+
+  (it "computes bounds for ::keyword"
+      (ict-with-assess-buffers
+       ((a (insert "::keyword")))
+       (with-current-buffer a
+         (expect (ict-bounds-string (inf-clojure-completion-bounds-of-expr-at-point))
+                 :to-equal "::keyword"))))
+
+  (it "computes bounds for [^:keyword (combined break chars and keyword)"
+      (ict-with-assess-buffers
+       ((a (insert "[^:keyword")))
+       (with-current-buffer a
+         (expect (ict-bounds-string (inf-clojure-completion-bounds-of-expr-at-point))
+                 :to-equal ":keyword"))))
+
+  (it "computes no bounds for point directly after a break expression"
+      (ict-with-assess-buffers
+       ((a (insert "@")))
+       (with-current-buffer a
+         (expect
+          (ict-bounds-string (inf-clojure-completion-bounds-of-expr-at-point))
+          :not :to-be nil))))
+
+  (it "computes bounds for [symbol"
+      (ict-with-assess-buffers
+       ((a (insert "[symbol")))
+       (with-current-buffer a
+         (expect (ict-bounds-string (inf-clojure-completion-bounds-of-expr-at-point))
+                 :to-equal "symbol"))))
+
+  (it "computes bounds for (@deref (multiple break chars)"
+      (ict-with-assess-buffers
+       ((a (insert "(@deref")))
+       (with-current-buffer a
+         (expect (ict-bounds-string (inf-clojure-completion-bounds-of-expr-at-point))
+                 :to-equal "deref")))))
+
+;;; inf-clojure-tests.el ends here
