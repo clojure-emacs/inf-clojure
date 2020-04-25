3c68ff4ccce3b3d2232989bfdc5d3c4aff3aef44
dan sutton <dan@dpsutton.com>
2020-04-25 11:11:33 -0500

    Mark this is not part of emacs. probably copy paste error

--- test/inf-clojure-tests.el
+++ test/inf-clojure-tests.el
@@ -8,7 +8,7 @@
 ;; Keywords: processes, clojure
 ;; Package-Requires: ((emacs "24.4") (clojure-mode "5.3"))
 
-;; This file is part of GNU Emacs.
+;; This file is not part of GNU Emacs.
 
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
