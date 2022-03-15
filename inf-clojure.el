f511dcad0e0684c9609c7526930e88d11da989ab
Bozhidar Batsov <bozhidar@batsov.dev>
2022-03-15 13:02:48 +0200

    Tweak docstring

--- inf-clojure.el
+++ inf-clojure.el
@@ -421,11 +421,12 @@ mode line entirely."
   :risky t)
 
 (defcustom inf-clojure-enable-eldoc t
-  "Var that allows disabling `eldoc-mode` in `inf-clojure`.
+  "Var that allows disabling `eldoc-mode' in `inf-clojure'.
 
-Set to `nil` to disable eldoc.  Eldoc can be quite useful by
+Set to nil to disable eldoc.  Eldoc can be quite useful by
 displaying function signatures in the modeline, but can also
-cause multiple prompts to appear and mess with `*1`, `*2`, etc."
+cause multiple prompts to appear in the REPL and mess with *1,
+*2, etc."
   :type 'boolean
   :safe 'booleanp
   :package-version '(inf-clojure . "3.2.0"))
